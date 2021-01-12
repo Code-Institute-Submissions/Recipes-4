795984bd3d31446c042d7aea95218060d6495537
sykez12 <adam_paddy@hotmail.co.uk>
2021-01-12 16:06:16 +0000

    Add Dish category Materialize select functionality

--- app.py
+++ app.py
@@ -102,7 +102,8 @@ def logout():
 
 @app.route("/add_dish")
 def add_dish():
-    return render_template("add_dish.html")
+    categories = mongo.db.categories.find().sort("category_name", 1)
+    return render_template("add_dish.html", categories=categories)
 
 
 if __name__ == "__main__":
